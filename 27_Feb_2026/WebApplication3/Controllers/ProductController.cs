d0c3e0571759c2e668eeab0fc6f77d7d05eb7b8e
Arrow-4587 <indreshmehta27@gmail.com>
2026-02-28 11:43:07 +0530

    first commit

---
+++ 27_Feb_2026/WebApplication3/Controllers/ProductController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication3.Models;
+
+namespace WebApplication3.Controllers
+{
+    public class ProductController : Controller
+    {
+        private static List<Product> products = new List<Product>()
+        {
+            new Product(){Id=1,Name="Laptop",Price=1000},
+            new Product(){Id=2,Name="Phone",Price=500},
+            new Product(){Id=3,Name="Tablet",Price=300}
+        };
+        public IActionResult Index()
+        {
+            return View(products);
+        }
+    }
+}
