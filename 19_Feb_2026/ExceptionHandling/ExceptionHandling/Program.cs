38408a24356addea2122611537196d38abd3c2f0
Arrow-4587 <indreshmehta27@gmail.com>
2026-04-07 11:32:12 +0530

    Assessment

--- 19_Feb_2026/ExceptionHandling/ExceptionHandling/Program.cs
+++ 19_Feb_2026/ExceptionHandling/ExceptionHandling/Program.cs
@@ -27,9 +27,9 @@
 
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+// using System.Linq;
+// using System.Text;
+// using System.Threading.Tasks;
 
 namespace CustomExceptionExampleCode
 {
@@ -44,4 +44,31 @@ namespace CustomExceptionExampleCode
         {
         }
     }
+    class Program {
+
+static void Main(string[] args) {
+
+int a = 50;
+
+int b = 10;
+
+int k = a / b;
+
+try {
+
+if (k < 10) {
+
+throw new MyException("value of k is less than 10");
+
 }
+
+} catch (MyException e) {
+Console.WriteLine("Caught My Exception");
+Console.WriteLine(e.Message);
+}
+
+Console.Read();
+
+  }
+ }
+ }
